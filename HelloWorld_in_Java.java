7b5b343918d65646a745c1a4fae19c8fa25276a6
tejastn10 <tejastn10@gmail.com>
2020-07-08 20:22:28 +0530

    📦 NEW: Add Java ad SQL files

---
+++ HelloWorld_in_Java.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello, World!");
+    }
+}
+
+// * Important info
+// ! Danger highlight
+// ? Plain info
+// TODO: todos
